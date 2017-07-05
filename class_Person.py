fbfd0783a38f8b34558bf71c7cab4d05786ddb78
steffj88 <steffj88@gmail.com>
2017-07-05 20:44:51 +0200

    Added first successful use of classes and instances
    *class_car.py
    *class_Person.py
    *learn_classes.py

---
+++ class_Person.py
@@ -0,0 +1,7 @@
+class Person(object):
+
+    def __init__(self, name, weight):
+        self.weight=weight
+        self.name=name
+
+        return
---
+++ learn_classes.py
@@ -0,0 +1,14 @@
+
+import class_Person
+
+person1=class_Person.Person('Steffen',50)
+
+if person1.weight > 40:
+    print (person1.weight)
+    print('The name of the object in question is', person1.name)
+
+else:
+        print('The person is probably underweight')
+
+
+
