fa8802b4e5fabf1c3ddd2ae2e9084e58e2ff000c
steffj88 <steffj88@gmail.com>
2017-07-05 21:51:40 +0200

    Add clas __doc__ description and call in file

--- class_Person.py
+++ class_Person.py
@@ -1,4 +1,5 @@
 class Person(object):
+    """The Person class represents people in a registry"""
 
     def __init__(self, name, weight):
         self.weight=weight
--- learn_classes.py
+++ learn_classes.py
@@ -3,6 +3,8 @@ import class_Person as cp
 
 person1=cp.Person('Steffen',50)
 
+print(cp.Person.__doc__)
+
 if person1.weight > 40:
     print (person1.weight)
     print('The name of the object in question is', person1.name)
